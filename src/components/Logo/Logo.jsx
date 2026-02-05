f9cd9dad09e982069df7af4cabfd4d0143a288cb
Shyine Makin <shyine@Shyines-MacBook-Pro.local>
2026-02-04 22:25:52 -0500

    created Logo component and modified

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,8 +1,10 @@
 import instockLogo from '../../assets/logo/InStock-Logo.svg'
+
 import './Logo.scss'
 
 function Logo() {
 return (
+    
     <img src={instockLogo} alt="InStock" className="logo" />
 )
 }
