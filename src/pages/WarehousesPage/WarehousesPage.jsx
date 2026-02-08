8a42649857c28884eb8eb0b63ea08f6eb924b48d
Sarra Drine <sarra.drine2@gmail.com>
2026-02-07 23:32:25 -0500

    create TextField (text input) and ErrorMessage components, and add default font weight in global sass partial

--- src/pages/WarehousesPage/WarehousesPage.jsx
+++ src/pages/WarehousesPage/WarehousesPage.jsx
@@ -1,8 +1,16 @@
 import "./WarehousesPage.scss";
+import TextField from "../../components/FormFields/TextField/TextField";
+import { useState } from "react";
 
 const WarehousesPage = ({ warehouses, setWarehouses }) => {
-console.log("WarehousesPage");
-
+    console.log("WarehousesPage");
+    const [value, setValue] = useState("");
+    const handleTextChange = (event) => {
+        setValue(event.target.name.value);
+    }
+    return (
+        <TextField onChange={handleTextChange} label="Item Name" name="name" id="name" placeholder="Item Name" value={value} error="This field is required" isError={true} />
+    );
 };
 
 export default WarehousesPage;
