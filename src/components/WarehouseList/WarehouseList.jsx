a0a22480cead11ec49e342cea4964c22d96a2a64
Shyine Makin <shyine@Shyines-MacBook-Pro.local>
2026-02-08 18:39:04 -0500

    modified WarehouseList jsx by adding prompt and importing WarehouseListItem

---
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -0,0 +1,10 @@
+import "./WarehouseList.scss";
+import WarehouseListItem from "./WarehouseListItem";
+
+const WarehousesPage = ({ warehouses, setWarehouses }) => {
+    
+    console.log("WarehousesList");
+    
+    };
+    
+    export default WarehousesList;
