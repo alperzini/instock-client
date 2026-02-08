988141e86fb1d4fe4f6a01ef1a6d58d9ed485fcf
Shyine Makin <shyine@Shyines-MacBook-Pro.local>
2026-02-08 18:44:01 -0500

    modified WarehouseList.jsx - created pageheader, search UI only and Add new warehouse button

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -1,10 +1,29 @@
 import "./WarehouseList.scss";
 import WarehouseListItem from "./WarehouseListItem";
 
-const WarehousesPage = ({ warehouses, setWarehouses }) => {
+const WarehousesPage = ({ warehouses, setWarehouses }) => {    
+    return (
+        <section className="warehouse-list">
+            {/* Page header */}
+            <div className="warehouse-list__header">
+            <h1 className="warehouse-list__title">Warehouses</h1>
     
-    console.log("WarehousesList");
+            <div className="warehouse-list__actions">
+                {/* Search (UI only) */}
+                <input
+                type="text"
+                className="warehouse-list__search"
+                placeholder="Search..."
+                />
     
+                {/* Add button */}
+                <button className="warehouse-list__add">
+                + Add New Warehouse
+                </button>
+            </div>
+            </div>
+        </section>
+        );
     };
     
     export default WarehousesList;
