5c90645b7b3fa45d7ae95fd431a868af01393920
Sarra Drine <sarra.drine2@gmail.com>
2026-02-09 13:48:29 -0500

    add SearchField component

--- src/pages/WarehousesPage/WarehousesPage.jsx
+++ src/pages/WarehousesPage/WarehousesPage.jsx
@@ -1,7 +1,7 @@
 import "./WarehousesPage.scss";
 import TextField from "../../components/FormFields/TextField/TextField";
-import QuantityField from "../../components/FormFields/QuantityField/QuantityField";
 import StatusField from "../../components/FormFields/StatusField/StatusField";
+import SearchField from "../../components/FormFields/SearchField/SearchField";
 import { useState } from "react";
 
 const WarehousesPage = ({ warehouses, setWarehouses }) => {
@@ -14,12 +14,15 @@ const WarehousesPage = ({ warehouses, setWarehouses }) => {
     const handleRadioChange = (event) => {
         setSelectedStatus(event.target.value);
     }
+    const [search, setSearch] = useState("");
+    const handleSearchChange = (event) => {
+        setSearch(event.target.name.value);
+    }
     return (
         <>
-            <TextField onChange={handleTextChange} type="email" label="Item Name" name="name" id="name" placeholder="Item Name" value={value} error="This field is required" isError={true} />
-            <QuantityField onChange={handleTextChange} label="Item Name" name="name" id="name" placeholder="Item Name" value={value} error="This field is required" isError={true} />
-            <QuantityField onChange={handleTextChange} label="Item Name" name="name" id="name" placeholder="Item Name" value={value} error="This field is required" isError={true} />
+            <TextField onChange={handleTextChange} type="email" label="Item Name" name="name" id="name" placeholder="Item Name" value={value} error="This field is required" isError={false} />
             <StatusField legend="Status" name="status" selectedStatus={selectedStatus} onChange={handleRadioChange} />
+            <SearchField onChange={handleSearchChange} name="search" id="search" placeholder="Search..." value={search} />
         </>
     );
 };
