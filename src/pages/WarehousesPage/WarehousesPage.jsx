bab076497fb4ef4a67a01336e93cbd042e91286d
Shyine Makin <shyine@Shyines-MacBook-Pro.local>
2026-02-12 18:27:50 -0500

    modified WarehousePage removed mock data for list hard coded in for next task

--- src/pages/WarehousesPage/WarehousesPage.jsx
+++ src/pages/WarehousesPage/WarehousesPage.jsx
@@ -1,59 +1,17 @@
-// import "./WarehousesPage.scss";
-// import WarehouseList from "../../components/WarehouseList/WarehouseList";
-
-// const WarehousesPage = ({ warehouses, setWarehouses }) => {
-
-// console.log("WarehousesPage");
-
-// return (
-//     <main className="warehouses-page">
-//         <WarehouseList warehouses={warehouses} />
-//     </main>
-// );
-
-// };
-
-// export default WarehousesPage;
-
-
 import "./WarehousesPage.scss";
 import WarehouseList from "../../components/WarehouseList/WarehouseList";
 import PageWrapper from "../../components/PageWrapper/PageWrapper"
-import { useEffect } from "react";
 
 const WarehousesPage = ({ warehouses, setWarehouses }) => {
 
-useEffect(() => {
-    // TEMP: mock data for testing Ticket 45
-    setWarehouses([
-    {
-        id: "1",
-        warehouse_name: "Toronto",
-        address: "123 King St",
-        city: "Toronto",
-        country: "Canada",
-        contact_name: "Ava Patel",
-        contact_phone: "+1 (416) 555-1234",
-        contact_email: "ava@instock.com",
-    },
-    {
-        id: "2",
-        warehouse_name: "Chicago",
-        address: "3218 Guess Rd",
-        city: "Chicago",
-        country: "USA",
-        contact_name: "Jameson Schuppe",
-        contact_phone: "+1 (919) 797-2875",
-        contact_email: "jschuppe@instock.com",
-    },
-    ]);
-}, [setWarehouses]);
-
 return (
+    <PageWrapper> 
     <main className="warehouses-page">
-    {/* <PageWrapper />  */}
     <WarehouseList warehouses={warehouses} /> 
     </main>
+    </PageWrapper> 
+
+
 );
 
     
