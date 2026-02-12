6297867612e8c4f6f9e46de3c0bcd7fba998c49b
Shyine Makin <shyine@Shyines-MacBook-Pro.local>
2026-02-12 18:28:06 -0500

    modified WarehousePage removed mock data for list hard coded in for next task

--- src/pages/WarehousesPage/WarehousesPage.jsx
+++ src/pages/WarehousesPage/WarehousesPage.jsx
@@ -1,21 +1,15 @@
 import "./WarehousesPage.scss";
 import WarehouseList from "../../components/WarehouseList/WarehouseList";
-import PageWrapper from "../../components/PageWrapper/PageWrapper"
+import PageWrapper from "../../components/PageWrapper/PageWrapper";
 
 const WarehousesPage = ({ warehouses, setWarehouses }) => {
-
-return (
-    <PageWrapper> 
-    <main className="warehouses-page">
-    <WarehouseList warehouses={warehouses} /> 
-    </main>
-    </PageWrapper> 
-
-
-);
-
-    
+  return (
+    <PageWrapper>
+      <main className="warehouses-page">
+        <WarehouseList warehouses={warehouses} />
+      </main>
+    </PageWrapper>
+  );
 };
-
 
 export default WarehousesPage;
