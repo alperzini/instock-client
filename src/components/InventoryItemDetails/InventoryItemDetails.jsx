6bf57657b543091565b0818dfebf293880ec1df0
Kshazly <karim.elshazly23@gmail.com>
2026-02-18 16:55:50 -0500

    feature-MM-57 scss and jsx fix

--- src/components/InventoryItemDetails/InventoryItemDetails.jsx
+++ src/components/InventoryItemDetails/InventoryItemDetails.jsx
@@ -2,7 +2,9 @@ import "./InventoryItemDetails.scss";
 
 import { useNavigate } from "react-router-dom";
 
-import BackArrowIcon from "../Icons/BackArrowIcon";
+import Button from "../Button/Button";
+import BackArrowButton from "../customButtons/BackArrowButton/BackArrowButton";
+
 import editIcon from "../../assets/icons/edit-24px.svg";
 
 const InventoryItemDetails = ({ item }) => {
@@ -12,33 +14,26 @@ const InventoryItemDetails = ({ item }) => {
   const isOut = statusLower.includes("out");
 
   return (
-    <div className="inventory-item-details">
-      <div className="inventory-item-details__header">
-        <div className="inventory-item-details__title-wrap">
-          <button
-            type="button"
-            className="inventory-item-details__back"
-            onClick={() => navigate(-1)}
-            aria-label="Go back"
-          >
-            <BackArrowIcon />
-          </button>
+    <section className="inventory-item-details">
+      <header className="inventory-item-details__header">
+        <div className="inventory-item-details__title-group">
+          <BackArrowButton />
           <h1 className="inventory-item-details__title">{item?.item_name}</h1>
         </div>
 
-        <button
-          type="button"
-          className="inventory-item-details__edit"
-          onClick={() => navigate(`/editInventory/${item.id}`)}
+        <Button
+          variant="primary"
+          className="inventory-item-details__edit-btn"
+          onClick={() => navigate(`/editInventory/${item?.id}`)}
         >
           <img className="inventory-item-details__edit-icon" src={editIcon} alt="" />
           <span className="inventory-item-details__edit-text">Edit</span>
-        </button>
-      </div>
+        </Button>
+      </header>
 
       <div className="inventory-item-details__divider" />
 
-      <div className="inventory-item-details__body">
+      <div className="inventory-item-details__content">
         <div className="inventory-item-details__left">
           <div className="inventory-item-details__block">
             <p className="inventory-item-details__label">ITEM DESCRIPTION:</p>
@@ -52,7 +47,7 @@ const InventoryItemDetails = ({ item }) => {
         </div>
 
         <div className="inventory-item-details__right">
-          <div className="inventory-item-details__top-grid">
+          <div className="inventory-item-details__top">
             <div className="inventory-item-details__block">
               <p className="inventory-item-details__label">STATUS:</p>
               <span
@@ -76,7 +71,7 @@ const InventoryItemDetails = ({ item }) => {
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
