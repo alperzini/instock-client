c973a1e156344e822efa332e41ddbcf13c859fb9
alper <alperzini@hotmail.com>
2026-03-17 23:42:39 -0700

    App.jsx removed the PORT and change the localhost fallback

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,7 @@ import Footer from './components/Footer/Footer.jsx';
 import PageNotFound from './components/PageNotFound/PageNotFound.jsx';
 
 function App() {
-  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-  const PORT = import.meta.env.VITE_PORT;
-
-  // const PORT = import.meta.env.VITE_BACKEND_PORT || 8080;
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8080";
 
   const [warehouses, setWarehouses] = useState([]);
   const [inventory, setInventory] = useState([]);
@@ -26,14 +23,14 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const warehousesRes = await axios.get(`${BACKEND_URL}${PORT}/warehouses`); {/* Split the inventory and warehouse to two seperate try, if one of it fails, the other can continue, we can also use Promise.all but for now this is okay I believe */ }
+        const warehousesRes = await axios.get(`${BACKEND_URL}/warehouses`);
         setWarehouses(warehousesRes.data);
       } catch (error) {
         console.error("Error fetching warehouses:", error);
       }
 
       try {
-        const inventoryRes = await axios.get(`${BACKEND_URL}${PORT}/inventories`);
+        const inventoryRes = await axios.get(`${BACKEND_URL}/inventories`);
         setInventory(inventoryRes.data);
       } catch (error) {
         console.error("Error fetching inventory:", error);
